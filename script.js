04ef3c512a76b2ea3610a79ed7ee25006a27043f
Jen Berry <jenberrymail@gmail.com>
2016-03-10 15:57:43 +0000

    add tooltip

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 $(document).ready(function(){
-	
+
 	var percentageElement = $('.percentage-amount');
 	var radioButtons = $(".lender-form input:radio");
 	radioButtons.change(function(){
 		var percentage = $(this).attr('data-percentage');
 		percentageElement.html(percentage);
+	});
+
+	$( document ).tooltip({
+		position: { my: "right top", at: "right+10 bottom+10" }
 	});
 
 });
